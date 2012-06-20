432e80cb93948729cb9794f065b974d565442874
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2012-06-20 21:06:15 +0200

    Add HeapSort and SorterImpl renaming

--- Sorter/src/fabian/sorter/impl/BubbleSortImpl.java
+++ Sorter/src/fabian/sorter/impl/BubbleSortImpl.java
@@ -8,9 +8,9 @@ public class BubbleSortImpl<T extends Comparable<T>> extends SorterImpl<T> {
 	public List<T> sort() {
 		T x = null;
 		// Linker rand in dem schon sortier ist
-		for (int i = 0; i < list.size() - 1; i++) {
+		for (int i = 0; i < list.size(); i++) {
 			// j geht jeweils von links nach rechts durch das array und tauscht
-			for (int j = list.size() - 1; j > i; j++) {
+			for (int j = list.size() - 1; j >= i; j--) {
 				if (list.get(j - 1).compareTo(list.get(j)) < 0) {
 					// tauschen
 					x = list.get(j - 1);
---
+++ Sorter/src/fabian/sorter/impl/HeapSortImpl.java
@@ -0,0 +1,13 @@
+package fabian.sorter.impl;
+
+import java.util.List;
+
+public class HeapSortImpl<T extends Comparable<T>> extends SorterImpl<T> {
+
+	@Override
+	public List<T> sort() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

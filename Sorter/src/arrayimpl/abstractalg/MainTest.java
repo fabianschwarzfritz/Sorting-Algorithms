2028178204e61be3b9b69c07134ff54be4eadce7
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2012-07-01 19:27:08 +0200

    Add ausgeglichens mischen. Does not work correctly yet

--- Sorter/src/arrayimpl/abstractalg/MainTest.java
+++ Sorter/src/arrayimpl/abstractalg/MainTest.java
@@ -2,9 +2,14 @@ package arrayimpl.abstractalg;
 
 import java.util.Arrays;
 
+import arrayimpl.sorterimpl.AusgeglichenesMischenImpl;
+
 public class MainTest {
 	public static void main(String[] args) {
-		SorterTester t = new SorterTester();
-		System.out.println(Arrays.toString(t.generateInverseSortedList(10)));
+		int[] arr = new int[] { 5, 3, 8, 0, 9, 1, 10 };
+		Sorter sorter = new AusgeglichenesMischenImpl();
+		sorter.setArr(arr);
+		int[] sort = sorter.sort();
+		System.out.println(Arrays.toString(sort));
 	}
 }
--- Sorter/src/arrayimpl/sorterimpl/AusgeglichenesMischenImpl.java
+++ Sorter/src/arrayimpl/sorterimpl/AusgeglichenesMischenImpl.java
@@ -1,5 +1,8 @@
 package arrayimpl.sorterimpl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import arrayimpl.abstractalg.Sorter;
 
 public class AusgeglichenesMischenImpl implements Sorter {
@@ -9,7 +12,44 @@ public class AusgeglichenesMischenImpl implements Sorter {
 	@Override
 	public int[] sort() {
 		// TODO implement ausgeglichenes Mischen
-		return null;
+		mergesort(arr);
+		return arr;
+	}
+
+	private void mergesort(int[] arr) {
+		// Array in 4 teile teilen
+		ArrayList<Integer> listA = new ArrayList<Integer>(arr.length);
+		ArrayList<Integer> listB = new ArrayList<Integer>(arr.length / 2);
+		ArrayList<Integer> listC = new ArrayList<Integer>(arr.length);
+		ArrayList<Integer> listD = new ArrayList<Integer>(arr.length / 2);
+
+		// partition on listc and listd
+		for (int i = 0; i < arr.length; i++) {
+			if (i % 2 == 0) {
+				listC.add(arr[i]);
+			} else {
+				listD.add(arr[i]);
+			}
+		}
+
+	}
+
+	private List<ArrayList<Integer>> mergeList(ArrayList<Integer> firstfromlist,
+			ArrayList<Integer> secondformlist) {
+		ArrayList<Integer> intofirst = new ArrayList<Integer>();
+		ArrayList<Integer> intosecond = new ArrayList<Integer>();
+		for (int i = 0; i < firstfromlist.size(); i++) {
+			if (i % 2 == 0) {
+				Integer firstfrom = firstfromlist.get(i);
+				Integer secondfrom = firstfromlist.get(i);
+				List<Integer> mergeBlock = mergeBlock(firstfrom, secondfrom);
+				
+			}
+		}
+
+	}
+
+	private List<Integer> mergeBlock(Integer firstfrom, Integer secondfrom) {
 	}
 
 	@Override
